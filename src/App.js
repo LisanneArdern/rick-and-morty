2f5ff7f6b9663cda2adff36aea375e1440b82450
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-05-14 11:48:55 +0200

    Add card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import characters from './data.json'
+import Card from './Card'
 import './App.css'
 
 export default function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      {characters.map(character => {
+        const { id, name } = character
+        return <Card key={id} name={name} />
+      })}
+    </div>
+  )
 }
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import './Card.css'
+
+export default function Card({ name }) {
+  return (
+    <section>
+      <h2>{name}</h2>
+    </section>
+  )
+}
