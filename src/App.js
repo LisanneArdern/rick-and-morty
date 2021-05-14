523a6cb3687428905de0e19cbd93b65e028a55ca
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-05-14 12:15:11 +0200

    Add flexible fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import characters from './data.json'
+import React, { useState, useEffect } from 'react'
 import Card from './Card'
 import './App.css'
 
 export default function App() {
+  const url = 'https://rickandmortyapi.com/api/character'
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(resBody => setCharacters(resBody.results))
+  }, [url])
   return (
     <div className="App">
       {characters.map(character => {
